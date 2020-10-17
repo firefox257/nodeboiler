36c634bc7bde12e0d80f41d21bda47a8adc35181
play.js <git@gituser.com>
2020-10-17 17:56:41 -0600

    Cleaned up some directories.

--- api/RegisterFactory.js
+++ api/RegisterFactory.js
@@ -5,7 +5,7 @@ include("Cors.js");
 include("Header.js");
 include("Requests/JsonRequest.js");
 include("Responses/JsonResponse.js");
-include("Exception.js");
+include("Exceptions/Exception.js");
 include("Routes.js");
 include("RegisterRoutes.js");
 
